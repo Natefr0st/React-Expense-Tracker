ffcd20ba54ad98935efb4044fc8016606e7f4111
Kevin <kevin_zino@yahoo.com>
2020-02-26 22:55:02 +0200

    Create Context Global State Reducer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AddTransaction = () => {
+    const [text, setText] = useState();
+    const [amount, setAmount] = useState();
+
     return (
         <div>
             <h3>Add new transaction</h3>
             <form>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text..." />
+                    <input
+                        type="text"
+                        placeholder="Enter text..."
+                        value={text}
+                        onChange={e => setText(e.target.value)}
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">
@@ -18,6 +26,8 @@ export const AddTransaction = () => {
                         type="number"
                         id="amount"
                         placeholder="Enter amount..."
+                        value={amount}
+                        onChange={e => setAmount(e.target.value)}
                     />
                 </div>
                 <button className="btn">Add transaction</button>
